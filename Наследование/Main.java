2dbefc394c2e18c3a6883ac152543cef06d898a0
Donnidarko321 <90192312+Donnidarko321@users.noreply.github.com>
2022-07-08 15:45:27 +0300

    Create Main.java

---
+++ Наследование/Main.java
@@ -0,0 +1,10 @@
+public class Main{
+	public static void main(String[] args) {
+		Dog bob=new Dog("Бобик","Гав-гав");
+		Dog2 polkan=new Dog2("Полкан","гав-гав");
+		Dog3 snoopy=new Dog3("Снупи","гав-гав");
+		bob.say();
+		polkan.say();
+		snoopy.say();
+	}
+  }
