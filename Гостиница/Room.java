ed9403f771e02d9244057f137f349fd02e6d2a30
Donnidarko321 <90192312+Donnidarko321@users.noreply.github.com>
2022-07-08 18:26:46 +0300

    Create Room.java

---
+++ Гостиница/Room.java
@@ -0,0 +1,17 @@
+public class Room{
+	int room;
+	boolean status;
+	Room(int a){
+		this.room=a;
+		this.status=false;
+	}
+	public int getRoom() {
+		return this.room;
+	}
+	public boolean getStatus() {
+		return this.status;
+	}
+	void setRoom(boolean a) {
+		status=a;
+	}
+}
