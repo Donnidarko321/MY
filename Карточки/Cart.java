fe41f3dbfc4414f36ed7bbc0f5fe5599ad3d1059
Donnidarko321 <90192312+Donnidarko321@users.noreply.github.com>
2022-07-14 23:39:26 +0300

    Create Cart.java

---
+++ Карточки/Cart.java
@@ -0,0 +1,15 @@
+public class Cart {
+	 int id;
+	 int price;
+	 Cart(int a,int b){
+		 this.id=a;
+		 this.price=b;
+	 }
+	public int getId() {
+		 return this.id;
+	 }
+	 public int getPrice() {
+		 return this.price;
+	 }
+	 
+}
